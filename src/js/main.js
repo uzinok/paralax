7b669f1c2968a093cdf9287c9a633a62659d88eb
uzinok <uzinok@yandex.ru>
2020-04-19 14:02:49 +0400

    paralax

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
-window.onload = function () {};
+window.onload = function () {
+    document.addEventListener("mousemove", function(e) {
+        e = e || event;
+        let mountains = document.querySelector('.mountains'), clouds = document.querySelector('.clouds');
+        mountains.style.transform = "translate(" + e.clientX * 0.01 + "px, " + e.clientY * 0.01 + "px)";
+        clouds.style.transform = "translate(-" + e.clientX * 0.005 + "px, -" + e.clientY * 0.005 + "px)";
+    })
+};
